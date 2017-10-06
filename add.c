f4ee9561fea0833689add64954fd3336b66ee08b
Benjamin Diamond <bendiamond1997@live.co.uk>
2017-10-06 13:45:57 +0100

    Update and rename README.md to add.c
    changed to a c file

---
+++ add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//First project
+//I like Poker & zombies & aaron's mum.
+//Check if this works.
+
+int main(){
+  int a = 1;
+  int b = 3;
+  int c;
+  
+  c = a + b;
+  printf("%d", c);
+
+}
